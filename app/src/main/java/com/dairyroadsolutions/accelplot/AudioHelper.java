851e34d83e9748b4e21bcebc1ffcc3be989461f9
MoreCoffee12 <brian.howard@ihmail.com>
2016-08-06 11:09:32 -0700

    Working on audio

--- app/src/main/java/com/dairyroadsolutions/accelplot/AudioHelper.java
+++ app/src/main/java/com/dairyroadsolutions/accelplot/AudioHelper.java
@@ -4,10 +4,14 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 
+import android.app.Activity;
+import android.util.Log;
+
+
 /**
  * Created by Brian on 8/5/2016.
  */
-public class AudioHelper {
+public class AudioHelper extends Activity {
 
     private int targetSamples = 1024;
     private int sample[] = new int[targetSamples];
@@ -20,11 +24,25 @@ public class AudioHelper {
 
     private boolean bAudioOut = false;
 
+    // debug
+    private static final String strTag = MainActivity.class.getSimpleName();
+
+
+    private void vUpdateAudio(){
+
+        UpdateFreq();
+        if( bAudioOut == true){
+            playSound();
+        }
+
+    }
+
     /**
      * toggle the audio out state
      */
     public void vToggleRunning(){
         bAudioOut = !bAudioOut;
+        vUpdateAudio();
     }
 
     /**
@@ -32,7 +50,9 @@ public class AudioHelper {
      * @param bAudioOutNew  True to turn on audio out
      */
     public void vSetAudioOut(boolean bAudioOutNew){
+
         bAudioOut = bAudioOutNew;
+        vUpdateAudio();
     }
 
     // From http://blog.workingsi.com/2012/03/android-tone-generator-app.html
@@ -59,7 +79,9 @@ public class AudioHelper {
 
         // calculate adjustments to make the sample start and stop evenly
         numCycles = (int)(0.5f + freqOfTone * (float)targetSamples/(float)sampleRate);
+        Log.d(strTag, ":HM:                            numCycles: " + numCycles);
         numSamples = (int)(0.5f + numCycles * (float)sampleRate/(float)freqOfTone);
+        Log.d(strTag, ":HM:                           numSamples: " + numSamples);
 
         // fill out the array
         for (int i = 0; i < numSamples; ++i) {
@@ -70,30 +92,30 @@ public class AudioHelper {
         // assumes the sample buffer is normalized.
         int idx = 0;
         for (double dVal : sample) {
-            // scale loudness by frequency
-            double amplitude = (double) (32767 * 5/(Math.log(freqOfTone)));
-            if (amplitude > 32767) amplitude = 32767;
-            // scale signal to amplitude
-            short val = (short) (dVal * amplitude);
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+//            // scale loudness by frequency
+//            double amplitude = (double) (32767 * 5/(Math.log(freqOfTone)));
+//            if (amplitude > 32767) amplitude = 32767;
+//            // scale signal to amplitude
+//            short val = (short) (dVal * amplitude);
+//            // in 16 bit wav PCM, first byte is the low order byte
+//            generatedSnd[idx++] = (byte) (val & 0x00ff);
+//            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
         }
     }
 
 
     void playSound(){
-        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, numSamples*2,
-                AudioTrack.MODE_STREAM);
-        audioTrack.write(generatedSnd, 0, numSamples*2);
-        audioTrack.play();
-        while (bAudioOut ==true){
-            audioTrack.write(generatedSnd, 0, numSamples*2);
-        }
-        audioTrack.stop();
-        bAudioOut = false;
+//        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+//                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+//                AudioFormat.ENCODING_PCM_16BIT, numSamples*2,
+//                AudioTrack.MODE_STREAM);
+//        audioTrack.write(generatedSnd, 0, numSamples*2);
+//        audioTrack.play();
+//        while (bAudioOut ==true){
+//            audioTrack.write(generatedSnd, 0, numSamples*2);
+//        }
+//        audioTrack.stop();
+//        bAudioOut = false;
 
     }
 
