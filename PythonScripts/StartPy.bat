ab51c306f0cf1f885efc7ad1063d7bd80277050d
Brian Howard <bhoward314159@gmail.com>
2022-01-02 12:20:44 -0800

    Cleaning up gitignore

--- PythonScripts/StartPy.bat
+++ PythonScripts/StartPy.bat
@@ -1,1 +1,15 @@
-jupyter notebook
+REM ---------------------------------------------------------------------------------
+REM This configuration is when Anaconda is installed for all users (not recommended!)
+REM ---------------------------------------------------------------------------------
+SET condaPath=%USERPROFILE%\Anaconda3\
+IF NOT EXIST %condaPath% SET condaPath=%USERPROFILE%\AppData\Local\Continuum\anaconda3\
+
+REM ---------------------------------------------------------------------------------
+REM Instantiate the notebook using the current directory
+REM ---------------------------------------------------------------------------------
+REM %USERPROFILE%"\Pycharms Env\pyenv3100\Scripts\Activate"
+%condaPath%python.exe %condaPath%cwp.py %condaPath% %condaPath%python.exe %condaPath%Scripts\jupyter-notebook-script.py "%~dp0
+
+pause
+
+
